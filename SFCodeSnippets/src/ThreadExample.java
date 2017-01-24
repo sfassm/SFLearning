3c96f2144d2a92030146caf0c2573d731430a4a8
fassmann@de.ibm.com <fassmann@de.ibm.com>
2017-01-24 08:57:04 +0100

    Initial commit. Program is working

---
+++ SFCodeSnippets/src/ThreadExample.java
@@ -0,0 +1,42 @@
+import java.io.IOException;
+
+/**
+ * This calls shows a separate thread is spawned for listing to keyboard
+ * hits.
+ * 
+ * @author stefan
+ *
+ */
+public class ThreadExample {
+	static boolean continueCounting = true;
+
+	public static void main(String[] args) {
+		// Kick off first thread: Wait for user to hit a key on the keyboard to exit the program
+		Thread waitForKeyboardPressThread = new Thread() {
+		    public void run(){
+				try {
+					System.out.println("Press Enter to shutdown connection and exit.");
+					System.in.read();
+					continueCounting = false;					
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+		    }
+		 };
+		 waitForKeyboardPressThread.start();
+		
+		 // Continue with main thread
+		int counter = 0;
+		while (continueCounting) {
+			System.out.println("Main thread continues counting every second: " + counter++);
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		System.out.println("You have hit the keyboard. We stop counting and the main thread.");
+	}
+
+}
